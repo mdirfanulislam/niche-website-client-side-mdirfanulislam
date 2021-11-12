b2cc961634dd3626143959e46fb66bf4afdf8ece
acc username <acc email>
2021-11-12 15:35:41 +0600

    added all

--- src/Pages/Dashboard/User/Review.js
+++ src/Pages/Dashboard/User/Review.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import { useForm } from "react-hook-form";
+import useAuth from './../../Hooks/Auth/useAuth';
 const Review = () => {
-    const { register, handleSubmit } = useForm();
-  const onSubmit = data => console.log(data);
+  const {user}=useAuth()
+    const { register, handleSubmit ,reset} = useForm();
+  const onSubmit = data =>{
+     console.log(data);
+     const confirmation =window.confirm('are you sure to submit this? ? ? ');
+     if(confirmation){
+       fetch('http://localhost:4000/reviews',{
+        method:"POST",
+        headers:{
+            "content-type":'application/json'
+        },
+        body:JSON.stringify(data)
+    })
+       .then(res=>res.json())
+       .then(data=>{
+         if(data.insertedId){
+           alert(' WE have got this');
+          reset()
+         };
+
+       })
+     }
+  };
     return (
         <div>
               <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("name", { required: true })} />
-      <input {...register("description", { required: true })} />
-      <input {...register("rating", { required: true })} />
+      <input className="mb-2" placeholder="your name" defaultValue={user.displayName} {...register("name", { required: true })} /> <br />
+      <input  className="mb-2" placeholder=" your experience" {...register("description", { required: true })} /> <br />
+      <input  className="mb-2" placeholder="Rating" {...register("rating", { required: true })} /> <br />
      
-      <input type="submit" />
+      <input type="submit" value="submit" />
     </form>
         </div>
     );
